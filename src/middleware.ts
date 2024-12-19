9bab26be063961193edecd6d042308eaa09740ef
Lav5588 <lavkumaryadavanshu@gmail.com>
2024-12-19 16:04:16 +0530

    nothin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 // export { default } from 'next-auth/middleware';
@@ -34,6 +33,3 @@ export async function middleware(request: NextRequest) {
 
     return NextResponse.next();
 }
-
-
-
